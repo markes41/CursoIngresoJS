ce6eb42cd0e39f42fb8d0056c3400c0ba48376d4
Markes41 <marcosemmanuelsequeira@outlook.com>
2018-07-05 10:10:51 -0300

    todo hecho

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,12 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var n1 = document.getElementById("importe").value;
+    var aumento1 = 100;
+    var porcentaje = 25;
+    var aumento2 = parseInt(n1) * parseInt(porcentaje);
+    var aumento3 = parseInt(aumento2) / parseInt(aumento1);
+    var aumentoFinal = parseInt(n1) - parseInt(aumento3);
+
+   document.getElementById("resultado").value = aumentoFinal;
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,13 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+    var n1 = document.getElementById("sueldo").value;
+    var aumento1 = 100;
+    var porcentaje = 10;
+    var aumento2 = parseInt(n1) * parseInt(porcentaje);
+    var aumento3 = parseInt(aumento2) / parseInt(aumento1);
+    var aumentoFinal = parseInt(n1) + parseInt(aumento3);
 
+    document.getElementById("resultado").value = aumentoFinal;
 	
 }
